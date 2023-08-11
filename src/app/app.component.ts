323b3005df6b39d968ad8556412f92c827fe58b6
ocket8888 <ocket8888@apache.org>
2023-08-11 14:17:06 -0600

    Add skills list to tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
  * The webapp root.
  */
 @Component({
-	selector: "app-root",
+	selector: "ocket8888-root",
 	styleUrls: ["./app.component.scss"],
 	templateUrl: "./app.component.html",
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button"
+import { HttpClientModule } from "@angular/common/http";
+import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatExpansionModule } from "@angular/material/expansion";
 import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
 import { MatTabsModule } from "@angular/material/tabs";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { SkillsComponent } from "./skills/skills.component";
 
 /**
  * AppModule is the only module in this simple app.
@@ -15,15 +19,19 @@ import { AppComponent } from "./app.component";
 @NgModule({
 	bootstrap: [AppComponent],
 	declarations: [
-		AppComponent
+		AppComponent,
+		SkillsComponent
 	],
 	imports: [
 		AppRoutingModule,
 		BrowserAnimationsModule,
 		BrowserModule,
+		HttpClientModule,
 		MatButtonModule,
 		MatCardModule,
+		MatExpansionModule,
 		MatIconModule,
+		MatListModule,
 		MatTabsModule,
 	],
 	providers: [],
