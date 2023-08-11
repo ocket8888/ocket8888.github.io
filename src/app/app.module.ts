6dfdb7d7bdbcfb38719d1648a32df9c6999f6df9
ocket8888 <ocket8888@apache.org>
2023-08-11 10:12:00 -0600

    Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /**
  * AppModule is the only module in this simple app.
@@ -14,7 +15,8 @@ import { AppComponent } from "./app.component";
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+  BrowserAnimationsModule
 	],
 	providers: [],
 })
