5f2e84577571a56a8bc18f301df299b135cf0830
ocket8888 <ocket8888@apache.org>
2023-08-11 11:13:07 -0600

    Add basic profile content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button"
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTabsModule } from "@angular/material/tabs";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /**
  * AppModule is the only module in this simple app.
@@ -14,9 +18,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 		AppComponent
 	],
 	imports: [
-		BrowserModule,
 		AppRoutingModule,
-  BrowserAnimationsModule
+		BrowserAnimationsModule,
+		BrowserModule,
+		MatButtonModule,
+		MatCardModule,
+		MatIconModule,
+		MatTabsModule,
 	],
 	providers: [],
 })
