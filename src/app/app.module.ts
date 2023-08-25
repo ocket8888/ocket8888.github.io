29ef762b3c92eedff2ad56863fc14ede27549257
ocket8888 <ocket8888@apache.org>
2023-08-24 18:17:27 -0600

    Fix up some formatting on the education page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
@@ -36,6 +37,7 @@ import { EducationComponent } from './education/education.component';
 		HttpClientModule,
 		MatButtonModule,
 		MatCardModule,
+		MatDividerModule,
 		MatExpansionModule,
 		MatIconModule,
 		MatListModule,
